cbaa8a3f5a22858b94da33211e48c330198dfc73
Knut Holtet <knutholtet@live.no>
2024-09-07 20:51:00 +0200

    Adding pre- and boilerplate code to add content to ListView

---
+++ my_app/lib/widgets/body/experience/experience.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Experience extends StatelessWidget {
+  const Experience({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+
+  }
+}
+
+
+
+
---
+++ my_app/lib/widgets/body/projects/projects.dart
@@ -0,0 +1,13 @@
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+class Projects extends StatelessWidget {
+  const Projects({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
