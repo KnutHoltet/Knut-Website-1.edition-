7e14e2826d06e18558470f1016ea2ddcac5c3641
Knut Holtet <knutholtet@live.no>
2024-09-07 23:02:31 +0200

    Adding files for cards implementation

---
+++ my_app/lib/widgets/body/experience/cards/cards.dart
@@ -0,0 +1,12 @@
+import 'package:catppuccin_flutter/catppuccin_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/*
+class Cards extends StatefulWidget {
+  const Cards({
+    super.key
+
+  });
+}
+ */
