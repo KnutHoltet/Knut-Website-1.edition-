c8cde2b1f438cabef16482cdc7d36eaa35a32161
Knut Holtet <knutholtet@live.no>
2024-09-09 14:21:46 +0200

    Removing dependencies for useless url library

--- my_app/lib/widgets/header/header.dart
+++ my_app/lib/widgets/header/header.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:simple_gradient_text/simple_gradient_text.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 
 
